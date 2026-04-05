93692d2719268acf8c46ff605f26005324fb8ff7
Suphanida Montreewiwat <admin@Gaggerhup.local>
2026-04-05 14:41:02 +0700

    Add PM2 ecosystem config

---
+++ ecosystem.config.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  apps: [
+    {
+      name: 'OneICU-Consulting',
+      cwd: '/root/OneICUConsultingSystem',
+      script: '/usr/bin/bash',
+      args: [
+        '-lc',
+        'cd /root/OneICUConsultingSystem && PORT=8000 node ./node_modules/next/dist/bin/next start -p 8000 -H 0.0.0.0',
+      ],
+      exec_mode: 'fork',
+      autorestart: true,
+      watch: false,
+      env: {
+        PORT: '8000',
+      },
+    },
+    {
+      name: 'OneICU-Bot',
+      cwd: '/root/OneICUConsultingSystem',
+      script: '/usr/bin/node',
+      args: ['telegram-bot/server.js'],
+      exec_mode: 'fork',
+      autorestart: true,
+      watch: false,
+    },
+  ],
+};
